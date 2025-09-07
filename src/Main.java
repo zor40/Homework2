dc72bb0ce23568c54a1577989c16480317783884
zor40 <zor40@mail.ru>
2025-09-07 17:39:26 +0500

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,82 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+         {
+
+
+             var dog = 8.0;
+             var cat = 3.6;
+             var paper = 763789;
+             System.out.println(dog);
+             System.out.println(cat);
+             System.out.println(paper);
+             dog = dog + 4;
+             System.out.println(dog);
+             cat = cat + 4;
+             System.out.println(cat);
+             paper = paper + 4;
+             System.out.println(paper);
+             dog = dog - 3.5;
+             System.out.println(dog);
+             cat = cat - 1.6;
+             System.out.println(cat);
+             paper = paper - 7639;
+             System.out.println(paper);
+             var friend = 19;
+             System.out.println(friend);
+             friend = friend + 2;
+             System.out.println(friend);
+             friend = friend / 7;
+             System.out.println(friend);
+             var frog = 3.5;
+             System.out.println(frog);
+             frog = frog * 10;
+             System.out.println(frog);
+             frog = frog / 3.5;
+             System.out.println(frog);
+             frog = frog + 4;
+             System.out.println(frog);
+             var oneBoxerWeight = 78.2;
+             System.out.println(oneBoxerWeight);
+             var twoBoxerWeight = 82.7;
+             System.out.println(twoBoxerWeight);
+             var boxersWeight = oneBoxerWeight + twoBoxerWeight;
+             System.out.println("Общий вес боксёров " + boxersWeight + " кг!");
+             var differenceWeight = twoBoxerWeight - oneBoxerWeight;
+             System.out.println("Разница веса боксёров " + differenceWeight + " кг!");
+             var remainder = twoBoxerWeight % oneBoxerWeight;
+             System.out.println("Остаток " + remainder);
+             var totalHours = 640;
+             System.out.println(totalHours);
+             var employeeHours = 8;
+             System.out.println(employeeHours);
+             var numberEmployees = totalHours / employeeHours;
+             System.out.println("Всего работников в компании " + numberEmployees);
+             numberEmployees = numberEmployees + 94;
+             System.out.println(numberEmployees);
+             var totalHours2 = numberEmployees * employeeHours;
+             System.out.println(totalHours2);
+             System.out.println("Если в компании работает 174 человека, то всего 1392 часа работы может быть поделено между сотрудниками. ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
